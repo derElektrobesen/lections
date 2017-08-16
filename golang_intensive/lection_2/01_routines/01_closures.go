852dc4dd451757f91c45ba5e2b5023f7afbaae41
Pavel Berezhnoy <pberejnoy2005@gmail.com>
2017-08-16 11:58:53 +0300

    some cases added

--- golang_intensive/lection_2/01_routines/01_closures.go
+++ golang_intensive/lection_2/01_routines/01_closures.go
@@ -30,6 +30,52 @@ func func1() {
 	fmt.Println("main: done")
 }
 
+func sleepInRoutine() {
+	for i := 0; i < 10; i++ {
+		go func(routineNo int) {
+			fmt.Println("Trying to sleep in goroutine ", routineNo)
+			time.Sleep(100 * time.Millisecond)
+			fmt.Println("Sleep done in goroutine ", routineNo)
+		}(i)
+	}
+}
+
+func func2() {
+	fmt.Println("Testing goroutines scope")
+	sleepInRoutine()
+	time.Sleep(2 * time.Second)
+	fmt.Println("Main function done")
+}
+
+type T struct {
+	A int
+}
+
+func (t T) fun() {
+	fmt.Println("Into T: A == ", t.A)
+}
+
+func (t *T) fun2() {
+	fmt.Println("Into T.func2: A == ", t.A)
+	t.A++
+}
+
+func func3() {
+	for _, t := range []T{T{1}, T{2}, T{3}} {
+		//go t.fun2()
+		go t.fun()
+		/*
+			go func() {
+				time.Sleep(10 * time.Millisecond)
+				t.fun()
+			}()
+		*/
+	}
+	time.Sleep(1 * time.Second)
+}
+
 func main() {
 	func1()
+	//func2()
+	//func3()
 }
